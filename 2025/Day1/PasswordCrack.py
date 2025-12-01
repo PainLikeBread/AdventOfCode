9a24e7565786d485992a8c924dec38506df0128b
Coons, Mitchell R <pigsplaypc@gmail.com>
2025-12-01 12:24:19 -0500

    starting part 2

--- 2025/Day1/PasswordCrack.py
+++ 2025/Day1/PasswordCrack.py
@@ -15,5 +15,23 @@ def crack_password(input):
             zeroCount += 1
     return zeroCount
 
+#print(crack_password(combos))
 
-print(crack_password(combos))
+def method0x434C49434B(input):
+    dial = 50
+    zeroCount = 0
+    for turn in input:
+        direction = turn[0]
+        amount = int(turn[1:])
+        if direction == "L":
+            dial -= amount
+            
+            print("direction: " + str(direction) + ", amount " + str(amount) + ", crossed zero? " + str(dial < 0) + ", Number of 0 crosses? " + ", turn count: " + str(dial))
+        else:
+            dial += amount
+            print("direction: " + str(direction) + ", amount " + str(amount) + ", crossed zero? " + str(dial > 100) + ", Number of 0 crosses? " + ", turn count: " + str(dial))
+        dial = dial % 100
+        print("true dial: " + str(dial))
+    return zeroCount
+
+print("PASSWORD: " + str(method0x434C49434B(combos)))
